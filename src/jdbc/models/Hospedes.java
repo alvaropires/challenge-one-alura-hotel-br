54599fea95f320bea50cf7ae27a34105f06a026e
Alvaro <alvaropires.eng@hotmail.com>
2023-05-03 17:01:27 -0300

    criado Connection Factory e models

---
+++ src/jdbc/models/Hospedes.java
@@ -0,0 +1,15 @@
+package jdbc.models;
+
+import java.sql.Date;
+
+public class Hospedes {
+	
+	private Integer id;
+	private String nome;
+	private String sobrenome;
+	private Date dataNascimento;
+	private String nacionalidade;
+	private String telefone;
+	private Reservas reserva;
+
+}
