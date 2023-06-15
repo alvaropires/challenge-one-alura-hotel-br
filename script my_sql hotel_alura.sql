c3e16e60518596b7565de95202dbcdf794f89548
Alvaro Pires Santos <alvaropires.eng@hotmail.com>
2023-06-15 19:20:51 -0300

    atualização no script sql para criação das tabelas
     Changes to be committed:
    	modified:   bin/.gitignore
    	modified:   script my_sql hotel_alura.sql

--- script my_sql hotel_alura.sql
+++ script my_sql hotel_alura.sql
@@ -22,23 +22,3 @@ reserva_id INT
 
 ALTER TABLE hospedes ADD CONSTRAINT fk_reservas FOREIGN KEY (reserva_id) references
 reservas(id);
-
-SELECT * FROM hospedes;
-
-INSERT INTO reservas (data_entrada, data_saida, valor, forma_pagamento) VALUES (
-'2023-05-05',
-'2023-05-06',
-'200',
-'cartão de débito'
-);
-
-INSERT INTO hospedes (nome, sobrenome, data_nascimento, nacionalidade, telefone, reserva_id) VALUES (
-'DEBORAH',
-'TELLES CARVALHO SANTOS',
-'1995-11-20',
-'brasileiro',
-'33999666699',
-2
-);
-
-SELECT h.nome, h.data_nascimento, h.telefone, r.data_entrada, r.data_saida, r.valor FROM hospedes AS h inner join reservas AS r on h.reserva_id = r.id;
